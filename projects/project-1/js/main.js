c43819232f2599b6e06ba4c8d72de2269aef1ecb
Vanessa <vanessa.y.cui@gmail.com>
2022-11-04 15:03:54 -0400

    adding pseudocode.txt file. adding new food gen. function. refactored code for board creation so divs have ids that make sense. modified reset btn function to remove food

--- projects/project-1/js/main.js
+++ projects/project-1/js/main.js
@@ -1,58 +1,27 @@
-// Pseudocode - MVP - snake game
-//1. define required variables used to track the state of the game
-//gameOn, speed of the game, spawn location of food (generated using rand nums between a range)
-//2. store elements on the page that will be accessed in code more than once into variables
-//overall grid, reset button, start button, etc.
-
-//Upon loading the webpage:
-//1. start button to show up in the middle of the grid.
-//2. start button to hide when user clicks on it & trigger the game to start.
-//3. render the square grid on the page, including the snake (coloured divs, 3 units long, middle of board)
-//4. Handle listening to keyboard arrow presses.
-//3. snake head moves in direction of arrow key pressed;
-//snake push/unshift snake array (array full of values that map to the board) to simulate movement
-//divs occupied by the snake will contain different class attributes.
-//(when snake is occupying a div, a class attribute will be added. when snake leaves, a class attribute will be deleted)
-//rate at which the classes to the div are added and subtracted is handled setInterval
-//4. when snake head touches a div containing a food class:
-// an element will be added to the beginning of the snake array
-//setInterval time will decrease (snake will move faster)
-//5. when snake head touches a div w/ a snake-body class:
-// game is over
-//6. when snake touches the border:
-//game is over
-//6. when game is over, board will not respond to keyboard presses anymore. (achieved using a gameOn state variable holding a boolean )
-//7. overlay w/ 'game over' message will cover screen. player will have to click on screen to dismiss. --> return back to step 1.
-//8. handle player clicking replay button --> an option for a player to reset the game --> return back to step 1.
-
-// Pseudocode - Additional Features to Consider:
-//1. highscore board:
-//before the game starts, input text box allows user to to enter their name before hitting start.
-//when the game is over, obtain length of snake array. Write to a text file witin the same project folder recording player name & high score.
-//when user loads the page, the highscore board will read the text file, sort from highest score to lowest score, extract the three highest score and display it
-//2. game difficulty:
-// add computer generated snake in addition to increasing snake speed in higher difficulty levels!
-//changes the color scheme of the game to light or dark.
-//4. player vs player:
-// adding additional keys to listen for in the game. two snakes will populate; one on each side of the board.
-// a function to check whether the head of a player's snake has hit itself or another person's snake.
-//5. turn sound on and off.
-
 //rendering game board, adding id number to square for reference purposes
-
 /*----- constants -----*/
 let boardSize = 30;
 let initSnakeLen = 3;
-let snakeStartPos = 435;
+let snakeStartPos = [14,14];
 
-for (let i = 0; i < boardSize * boardSize; i++) {
-  let square = document.createElement("div");
-  square.classList.add("cell");
-  square.setAttribute("id", "div" + i);
-  document.querySelector(".game-board").appendChild(square);
-}
+//creating board matrix AND creating divs
+let boardMatrix = [];
+for (let x = 0; x< boardSize; x++){
+  let rowArr = [];
+  for (let y = 0; y< boardSize; y ++){
+    rowArr.push(0);
+    let square = document.createElement("div");
+    square.classList.add("cell");
+    square.setAttribute("id", x+'-'+y);
+    document.querySelector(".game-board").appendChild(square);
+  }
+  boardMatrix.push(rowArr)
+} 
+
 /*----- app's state (variables) -----*/
 let gameOn = true;
+let snakeBody = [];
+let snakeHead = snakeBody[0]
 
 /*----- cached element references -----*/
 let startBtn = document.querySelector("#start-btn");
@@ -66,6 +35,7 @@ startBtn.addEventListener("click", function (event) {
   event.preventDefault();
   event.target.style.display = "none";
   snakeInit();
+  genFood();
 });
 
 resetBtn.addEventListener("click", function (event) {
@@ -73,20 +43,38 @@ resetBtn.addEventListener("click", function (event) {
   startBtn.style.display = "inline";
 
   //reset board.
-  gameSqs.forEach(sq=> sq.classList.remove("snake-body") )
+  gameSqs.forEach(sq=> sq.classList.remove("snake-body", "food") )
 
 });
+
+
+// //check div populated properly
+// gameBoard.addEventListener("click", function (event) {
+//   if (event.target.nodeName === 'DIV'){
+//     console.dir(event.target)
+//   }
+// });
 
 
 /*----- functions -----*/
 
 function snakeInit() {
   for (let i = 0; i< initSnakeLen; i++){
-     let identifier = "#div" + (snakeStartPos-i)
-     document.querySelector(identifier).classList.add("snake-body");
+    let identifier = (snakeStartPos[0]).toString() + '-' + (snakeStartPos[1]-i).toString()
+    snakeBody.push([snakeStartPos[0],snakeStartPos[1]-i ])
+    document.getElementById(identifier).classList.add("snake-body");
   }
 }
 
-function genFood(){
 
+function genFood(){
+  //make sure food doesnt spawn on snake body. 
+  let randomPos = [Math.floor(Math.random()*30), Math.floor(Math.random()*30)]
+  while (snakeBody.includes(randomPos)){
+    randomPos = [Math.floor(Math.random()*30), Math.floor(Math.random()*30)]
+  }
+  let foodid = (randomPos[0]).toString() + '-' + (randomPos[1]).toString()
+  document.getElementById(foodid).classList.add("food");
 }
+
+
